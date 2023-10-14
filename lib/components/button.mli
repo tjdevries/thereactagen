1752cb23ce67c2c2565de3ad9f3a0eb73904b3eb
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-10-14 11:51:43 -0400

    Remove commented code

--- lib/components/button.mli
+++ lib/components/button.mli
@@ -25,4 +25,3 @@ val make
   -> children:children
   -> unit
   -> Html_types.button elt
-(* "rounded px-4 py-2 text-slate-500 hover:bg-slate-200 active:scale-95 active:bg-slate-300 dark:text-slate-400 dark:hover:bg-slate-700 dark:active:bg-slate-600" *)

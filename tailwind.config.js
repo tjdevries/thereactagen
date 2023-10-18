8d3a90f6972128de4511883d2d0392c2814e0e6b
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-10-18 10:17:17 -0400

    initial work on adding a dune rule for building/compiling tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./lib/**/*.ml", "./lib/bin/**/*.ml"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
